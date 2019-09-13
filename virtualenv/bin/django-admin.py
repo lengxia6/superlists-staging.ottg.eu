fb7f56c149253fa4cc7349c8052bdf455ea42351
ubuntu <ubuntu@localhost.localdomain>
2019-09-13 14:16:26 +0800

    代码已成功部署在服务器

---
+++ virtualenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ubuntu/sites/superlists-staging.ottg.eu/virtualenv/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
